2b882c0d37d80d4e5b7d8c4c70c4e2260a2905b6
hriju <hriju.rahaman@gmail.com>
2020-09-09 19:19:57 +0600

    Project initialized

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Elearning</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+    <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+    <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+    <div id="header" class="container">
+        <div id="logo">
+            <h1><a href="#">Elearning</a></h1>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a href="#" accesskey="1" title="">Homepage</a></li>
+                <li><a href="#" accesskey="2" title="">Courses</a></li>
+                <li><a href="#" accesskey="3" title="">Login</a></li>
+                <li><a href="#" accesskey="4" title="">Registration</a></li>
+                <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="banner" class="container">
+        <p>This is <strong>Elearning</strong>, a free, learning site for Developers . In this site you will be able to learn your favorite courses and complete it by answering the questions under each section.
+    </div>
+</div>
+<div id="page-wrapper">
+    <div id="featured" class="container">
+        <div class="title">
+            <h2>Pellentesque viverra vulputate</h2>
+        </div>
+        <div class="tbox1">
+            <div class="padding-bottom">
+                <h2>Integer gravida nibh quis</h2>
+                <img src="images/img01.jpg" alt="" />
+                <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum.</p>
+                <a href="#" class="button">Etiam posuere</a>
+            </div>
+            <div>
+                <h2>Integer gravida nibh quis</h2>
+                <img src="images/img02.jpg" alt="" />
+                <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum.</p>
+                <a href="#" class="button">Etiam posuere</a>
+            </div>
+        </div>
+        <div class="tbox2">
+            <div class="padding-bottom">
+                <h2>Integer gravida nibh quis</h2>
+                <img src="images/img03.jpg" alt="" />
+                <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum.</p>
+                <a href="#" class="button">Etiam posuere</a>
+            </div>
+            <div>
+                <h2>Integer gravida nibh quis</h2>
+                <img src="images/img04.jpg" alt="" />
+                <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum.</p>
+                <a href="#" class="button">Etiam posuere</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="copyright">
+    <p>&copy; ELEARNING. All rights reserved. |  Design by HRIJU</p>
+</div>
+</body>
+</html>
